73cdd1508864a51d55500fb7fb24469b208934cd
evenwu <evendesign@gmail.com>
2013-04-13 03:15:27 +0800

    save

--- javascripts/site.js
+++ javascripts/site.js
@@ -4,4 +4,17 @@ $("body").click(function () {
   $(this).toggleClass("hide-grid");
 });
 
+init();
+
+$(window).resize(function(){
+  init();
 });
+
+});
+
+function init(){
+  var baseWidth = $(".square").width()
+  $(".square").css( "height", baseWidth );
+  $(".left-top").css( "border-width", baseWidth + "px " + baseWidth + "px 0 0");
+  $(".circle-top").css( "height", baseWidth / 2 );
+}
